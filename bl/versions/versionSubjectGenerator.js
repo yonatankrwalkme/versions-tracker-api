db374aa0b0737f9cc8137ca1b530224a3f6d84a6
Yonatan Krieger <yonatan.k@walkme.com>
2018-07-18 15:47:14 +0300

    fixing title generator

--- bl/versions/versionSubjectGenerator.js
+++ bl/versions/versionSubjectGenerator.js
@@ -1,11 +1,12 @@
 exports.generate = (version) => {
+    const title = `${version.projectName} - ${version.versionId}`;
     if (version.status.toLowerCase() === "init")
-        return `${version.projectName} - STARTING DEPLOYMENT`;
+        return `${title} - STARTING DEPLOYMENT`;
     if (version.status.toLowerCase() === "deploying")
-        return `${version.projectName} - PASSED BETA - DEPLOYING TO PRODUCTION`;
+        return `${title} - PASSED BETA - DEPLOYING TO PRODUCTION`;
     if (version.status.toLowerCase() === "complete")
-        return `${version.projectName} Version ${version.versionId} - deployment COMPLETE`;
+        return `${title} - DEPLOYMENT COMPLETE`;
     if (version.status.toLowerCase() === "fail")
-        return `${version.projectName} Version ${version.versionId} - FAILED to deploy`;
+        return `${title} - FAILED TO DEPLOY`;
     return `${version.projectName} - ${version.status.toUpperCase()}.`;
 };
