4e83d314bb17eb767addb8257bebd13bb078825f
Yonatan Krieger <yonatan.k@walkme.com>
2018-08-29 14:26:24 +0300

    removing email on init

--- bl/versions/versionEventHandler/versionInitEventHandler.js
+++ bl/versions/versionEventHandler/versionInitEventHandler.js
@@ -4,13 +4,13 @@ const versionSubjectGenerator = require('../versionSubjectGenerator');
 const configValueProvider = require('../../../services/configValueProvider');
 
 exports.handle = (version) => {
-    if (version.status !== "init")
+    // if (version.status !== "init")
         return Promise.resolve();
 
-    return emailer.sendMail(
-        `version-status`,
-        {version},
-        versionRecipientsCreator.generate(version),
-        versionSubjectGenerator.generate(version),
-        configValueProvider.getValue("EMAILING_SENDER"))
+    // return emailer.sendMail(
+    //     `version-status`,
+    //     {version},
+    //     versionRecipientsCreator.generate(version),
+    //     versionSubjectGenerator.generate(version),
+    //     configValueProvider.getValue("EMAILING_SENDER"))
 };
