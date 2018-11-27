379c00addfbe8c9b6a00e7b74043f342d507d66c
Yonatan Krieger <yonatan.k@walkme.com>
2018-11-27 17:59:04 +0200

    fixing images

--- jenkins/push-image.sh
+++ jenkins/push-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t bluehotdog/versions_tracker_client .
+docker build -t bluehotdog/versions_tracker_api .
 
-docker push  bluehotdog/versions_tracker_client
+docker push  bluehotdog/versions_tracker_api
